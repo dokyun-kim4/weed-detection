e674bff8094d9c66f9032ca332aaebcf2250f3ff
Dokyun Kim <donny5557@gmail.com>
2024-02-23 17:32:41 -0500

    organize function constants

--- helpers.py
+++ helpers.py
@@ -11,15 +11,16 @@ def get_green(orig_img: np.ndarray) -> np.ndarray:
     Returns:
         green_areas: numpy array representing the green-isolated image
     """
+
+    # low/high HSV limits
+    LOWER_GREEN = np.array([30,40,30])
+    UPPER_GREEN = np.array([100,255,255])
+
     # Convert the image from BGR to HSV color space
     rgb_image = cv2.cvtColor(orig_img, cv2.COLOR_BGR2HSV)
 
-    # Define the lower and upper bounds for the green color in HSV
-    lower_green = np.array([30,40,30])
-    upper_green = np.array([100,255,255])
-
     # Create a mask to isolate the green areas
-    mask = cv2.inRange(rgb_image, lower_green, upper_green)
+    mask = cv2.inRange(rgb_image, LOWER_GREEN, UPPER_GREEN)
 
     # Apply the mask to the original image
     green_areas = cv2.bitwise_and(orig_img, orig_img, mask=mask)
