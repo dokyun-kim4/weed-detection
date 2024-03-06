c80ba4e439f6a0f51aa2ecaf5513a0cdfc9c3a38
dfriishecht <dfriishecht@olin.edu>
2024-03-05 20:18:44 -0500

    Add pl@ntnet API call helper functions

--- modules/plantnet_helpers.py
+++ modules/plantnet_helpers.py
@@ -1,1 +1,26 @@
 # Functions for Pl@ntNet API
+
+import requests
+import json
+from pprint import pprint
+import cv2
+
+def API_Initialize(API_Key, Region):
+    api_endpoint = f"https://my-api.plantnet.org/v2/identify/{Region}?api-key={API_Key}"
+    return api_endpoint
+
+def load_plant_data(Img_Path, Organ):
+    path = Img_Path
+    image_data = open(path, 'rb')
+    data = { 'organs': [Organ] }
+    files = [('images', (path, image_data)),]  
+    return data, files
+
+def Send_API_Request(url, files, data):
+    req = requests.Request('POST', url=url, files=files, data=data)
+    prepared = req.prepare()
+    s = requests.Session()
+    response = s.send(prepared)
+    json_result = json.loads(response.text)
+    pprint(response)
+    return json_result
