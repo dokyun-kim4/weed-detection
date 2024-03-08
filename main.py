1747502218d52861cda201f6a8a3cf03cd6fbc5e
dfriishecht <dfriishecht@olin.edu>
2024-03-08 12:12:08 -0500

    Add performance benchmarkers to code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
 import os
+import time
 from dotenv import load_dotenv
 
 # Custom helpers
@@ -12,8 +13,7 @@ import modules.plantnet_helpers as ph
 SAFE_PLANTS = ["Lettuce"]
 
 load_dotenv()
-endpoint = ph.API_Initialize(os.getenv("KEY"))
-
+endpoint = ph.API_Initialize(os.getenv("2b109rJwhiLaSgff3H6wVCoT3u"))
 # Define DBscan model
 dbscan_model = DBSCAN(eps=5, min_samples=10)
 
@@ -23,7 +23,8 @@ img_copy = np.copy(image)
 
 # TODO
 # video camera trigger
-
+tic = time.perf_counter()
+print("Denoise Started")
 # Get green areas and remove noise
 green_areas = ch.get_green(image)
 bnw_image = ch.green_to_bnw(green_areas)
@@ -39,14 +40,22 @@ img_denoised[img_denoised < 150] = 0
 
 white_points = ch.binary_to_cartesian(img_denoised)
 
+toc = time.perf_counter()
+print(f"Denoise Complete: {toc-tic:0.4f} seconds, Starting Clustering")
+tic = time.perf_counter()
 # ----- Perform DBscan -------
 clusters = ch.DBSCAN_clustering(white_points)
-
+toc = time.perf_counter()
+print(f"Clustering Complete: {toc-tic:0.4f} seconds, Starting Bbox Locating")
 # Find bbox and center points
+tic = time.perf_counter()
 bboxes = [(bbox[0], bbox[1]) for bbox in ch.find_bounding_boxes(white_points, clusters)]
 centers = [center for center in ch.find_cluster_centers(white_points, clusters)]
-
+toc = time.perf_counter()
+print(f"Bounding Boxes Located: {toc-tic:0.4f} seconds, Starting API Request")
 # Iterate through bbox list, call GET request on plantnet
+print("Requesting Pl@ntNet API")
+tic = time.perf_counter()
 for bbox in bboxes:
     segmented_img = ch.return_image_array(bbox, img_copy, min_size=10000)
     if segmented_img is None:
@@ -56,3 +65,6 @@ for bbox in bboxes:
         data, files = ph.load_plant_data(img_buffer, Organ=["leaf"])
         result = ph.Send_API_Request(endpoint, files, data)
         print(result["results"][0]["species"]["commonNames"])
+        print(bbox)
+toc = time.perf_counter()
+print(f"Request Returned {toc-tic:0.4f} seconds")
